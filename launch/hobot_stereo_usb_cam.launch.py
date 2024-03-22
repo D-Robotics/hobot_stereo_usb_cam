e4286c83bc8eaea00d40f49473ef6d630d713f74
kao.zhu <kao.zhu@horizon.ai>
2024-03-22 14:09:35 +0800

    Include hobot_shm.launch.py in launch scripts

--- launch/hobot_stereo_usb_cam.launch.py
+++ launch/hobot_stereo_usb_cam.launch.py
@@ -16,6 +16,10 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python import get_package_share_directory
+import os
 
 
 def generate_launch_description():
@@ -48,6 +52,13 @@ def generate_launch_description():
             default_value='0',
             description='video device index'),
 
+        # 启动零拷贝环境配置node
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('hobot_shm'),
+                    'launch/hobot_shm.launch.py'))
+        ),
         Node(
             package='hobot_stereo_usb_cam',
             executable='hobot_stereo_usb_cam',
